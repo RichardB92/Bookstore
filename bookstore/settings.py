b04d006fbf26df3872ec781bd08f3c635c5c73fc
Richard GomezZuluaga <nosequecua@gmail.com>
2019-08-29 12:24:49 -0500

    bootstrap3 refactor added compression

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'registration',
+	'bootstrap3',
+	'bootstrap_themes',
+	'compressor',
 	'store',
 )
 
@@ -108,6 +111,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+COMPRESS_ENABLED = True
+STATICFILES_FINDERS = (
+	'django.contrib.staticfiles.finders.FileSystemFinder',
+	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+	'compressor.finders.CompresorFinder',
+)
 
 #registration
 ACCOUNT_ACTIVATION_DAYS = 7
